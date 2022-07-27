c852b0dee647441318289c04b73ae5c9ae36e9c4
Mercy Kangor <41419454+Mkangor97@users.noreply.github.com>
2022-07-27 07:08:58 +0300

    Update 101-lazy_matrix_mul.py
    Task driven development

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-# 101-lazy_matrix_mul.py
-"""Defines a matrix multiplication function using NumPy."""
+"""
+Module that multiply matrix using numpy module (pip3 install numpy)
+https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.matmul.html
+"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-    """
-
-    return (np.matmul(m_a, m_b))
+    """Returns matrix multiplied"""
+    return np.matmul(m_a, m_b)
