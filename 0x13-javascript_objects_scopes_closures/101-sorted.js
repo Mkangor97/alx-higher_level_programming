bac7d1e9a07ac2fce2a2653cb3412c4509f654b0
Mkangor97 <mercykangor@gmail.com>
2022-10-16 22:19:36 -0700

    Tasks

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
-=======
 const dict = require('./101-data').dict;
 const dKeys = Object.keys(dict);
 const values = Object.values(dict);
@@ -18,6 +9,5 @@ for (let i = 0; i < values.length; i++) {
   result[JSON.stringify(values[i])] = [];
   matched = dKeys.filter(key => dict[key] === values[i]);
   matched.forEach(item => result[JSON.stringify(values[i])].push(item));
->>>>>>> 7fc2773aa158efbf92cede0ed1b4df03a3e8fda8
 }
 console.log(result);
