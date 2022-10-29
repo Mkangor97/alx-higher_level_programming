1fad0c6f55d19f84c148a5c5a4700320d4a71203
Mkangor97 <mercykangor@gmail.com>
2022-10-29 01:07:20 -0700

    tasks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
-import requests
+"""A python script that fetches https://alx-intranet.hbtn.io/status"""
 
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+if __name__ == '__main__':
+    import requests
+    url = 'https://alx-intranet.hbtn.io/status'
+    r = requests.get(url)
+    body = r.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
